1921a8de9cf8bd47cb391afcc3686c2e6e266e87
Herman Martinus <hfbmartinus@gmail.com>
2019-01-16 21:13:45 +0200

    Some proper copying

--- src/js/main.js
+++ src/js/main.js
@@ -20,13 +20,8 @@ function ShowPassword () {
   }
 }
 
-function CopyToClipboard (e) {
-  const el = document.createElement('textarea')
-  el.value = thingthang
-  document.body.appendChild(el)
-  el.select()
-  document.execCommand('copy')
-  document.body.removeChild(el)
+function CopyToClipboard () {
+  copyTextToClipboard(generatedPassword.value)
   copyButton.innerText = 'Copied to clipboard!'
   setTimeout(function () {
     copyButton.innerText = 'Copy to clipboard'
@@ -57,6 +52,35 @@ function Generate (seed, service) {
 
 function ClickSelect (e) {
   e.select()
+}
+
+function fallbackCopyTextToClipboard (text) {
+  var textArea = document.createElement('textarea')
+  textArea.value = text
+  document.body.appendChild(textArea)
+  textArea.focus()
+  textArea.select()
+
+  try {
+    var successful = document.execCommand('copy')
+    var msg = successful ? 'successful' : 'unsuccessful'
+    console.log('Fallback: Copying text command was ' + msg)
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err)
+  }
+
+  document.body.removeChild(textArea)
+}
+function copyTextToClipboard (text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text)
+    return
+  }
+  navigator.clipboard.writeText(text).then(function () {
+    console.log('Async: Copying to clipboard was successful!')
+  }, function (err) {
+    console.error('Async: Could not copy text: ', err)
+  })
 }
 
 (function () {

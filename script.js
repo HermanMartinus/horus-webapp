05cf0a1ccc7cfbaa018a97b56ee6574e21c8219a
Herman Martinus <hfbmartinus@gmail.com>
2022-02-07 16:37:08 +0200

    Ignore empties

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
 function createPassword() {
-  const seed = document.querySelector('#seed').value;
-  const service = document.querySelector('#service').value;
-  document.querySelector('#passphrase').value = generatePassword(seed, service)
+  const seed = document.querySelector("#seed").value
+  const service = document.querySelector("#service").value
 
-  document.querySelector('.result').style.display = "block"
-  document.querySelector('.result label').innerText = `Password for ${service}`
+  if (!seed || !service) return
+
+  document.querySelector("#passphrase").value = generatePassword(seed, service)
+
+  document.querySelector(".result").style.display = "block"
+  document.querySelector(".result label").innerText = `Password for ${service}`
 }
 
 function createPassphrase() {
-  const seed = document.querySelector('#seed').value;
-  const service = document.querySelector('#service').value;
-  document.querySelector('#passphrase').value = generatePassphrase(seed, service)
+  const seed = document.querySelector("#seed").value
+  const service = document.querySelector("#service").value
 
-  document.querySelector('.result').style.display = "block"
-  document.querySelector('.result label').innerText = `Password for ${service}`
+  if (!seed || !service) return
+
+  document.querySelector("#passphrase").value = generatePassphrase(
+    seed,
+    service
+  );
+
+  document.querySelector(".result").style.display = "block"
+  document.querySelector(".result label").innerText = `Password for ${service}`
 }
 
 function generatePassphrase(seed, service) {
-  var combo = (service + seed).toLowerCase();
-  var hashObject = MD5(combo);
-  var digitList = hashObject.replace(/\D/g,'').match(/.{1,4}/g);
-  var words = [];
+  const combo = (service + seed).toLowerCase()
+  const hashObject = MD5(combo)
+  const digitList = hashObject.replace(/\D/g, "").match(/.{1,4}/g)
+  const words = []
   for (let x of digitList) {
-    words.push(wordList()[parseInt(x) % 2048]);
+    words.push(wordList()[parseInt(x) % 2048])
   }
 
-  let generated = words.slice(0, 4).join('-')
-  generated = generated.charAt(0).toUpperCase() + generated.slice(1) + '-1';
-  return generated;
+  let generated = words.slice(0, 4).join("-")
+  generated = generated.charAt(0).toUpperCase() + generated.slice(1) + "-1"
+  return generated
 }
 
-function generatePassword (seed, service) {
-  const specialCharacters = '!@#$%^&*()/'
-  if (seed.length > 0 && service.length > 0) {
-    const combo = service.toLowerCase() + seed.toLowerCase()
-    const hashObject = MD5(combo)
-    let generated = specialCharacters[service.length % 10]
-    generated += hashObject.substr(0, 7).toUpperCase()
-    generated += hashObject.substr(8, 7).toLowerCase()
-    generated += specialCharacters[10 - (service.length % 10)]
-    return generated
-  }
-  return ''
+function generatePassword(seed, service) {
+  const specialCharacters = "!@#$%^&*()/"
+
+  const combo = service.toLowerCase() + seed.toLowerCase()
+  const hashObject = MD5(combo)
+  let generated = specialCharacters[service.length % 10]
+  generated += hashObject.substr(0, 7).toUpperCase()
+  generated += hashObject.substr(8, 7).toLowerCase()
+  generated += specialCharacters[10 - (service.length % 10)]
+  return generated
 }
